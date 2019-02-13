832157a7c75fbdba40a9f173f1d2088d1ef6433b
waegaein <waegaein@gmail.com>
2019-02-13 21:41:50 +0900

    Run method

--- 15540/scratch.py
+++ 15540/scratch.py
@@ -1,6 +1,4 @@
-m = 2002
-a = 4
-b = 11
+import sys
 
 
 def is_prime(num):
@@ -15,6 +13,7 @@ def is_prime(num):
 		if num % f == 0: return False
 		if num % (f + 2) == 0: return False
 		f += 6
+
 	return True
 
 
@@ -25,6 +24,7 @@ def get_p_range_rev(m):
 def get_q_range_rev(m, a, b, p):
 	upper_bound_by_product = int(m / p)
 	upper_bound_by_ratio = int(b/a * p)
+
 	return reversed(range(p, min(upper_bound_by_product, upper_bound_by_ratio) + 1))
 
 
@@ -41,4 +41,18 @@ def get_most_suitable(m, a, b):
 	return max(res, key=lambda triplet: triplet[2])[:2]
 
 
-print(get_most_suitable(m, a, b))
+def run():
+	res = list()
+	for line in sys.stdin:
+		line_split = line.split(' ')
+		m = int(line_split[0])
+		a = int(line_split[1])
+		b = int(line_split[2])
+		if m * a * b != 0:
+			p, q = get_most_suitable(m, a, b)
+			res.append(str(p) + ' ' + str(q))
+
+	return '\n'.join(res)
+
+
+run()
