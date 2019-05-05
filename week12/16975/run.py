a428abebaa37028f4b1baf373b27007dae6490fe
waegaein <waegaein@gmail.com>
2019-05-05 21:33:42 +0900

    Pass 16975

--- week12/16975/run.py
+++ week12/16975/run.py
@@ -5,26 +5,19 @@ def parse_input():
     # with open('test.txt', 'r') as f:
     return [[int(num) for num in line.strip().split()] for line in f.readlines()[1:]]
 
-def apply_query_1(sequence, i, j , k):
-    for index in range(i-1, j):
-        sequence[index] += k
-
-    return sequence
-
-def apply_query_2(sequence, index):
-    print(sequence[index-1])
-
-    return
-
 def run():
     inputs = parse_input()
     sequence = inputs[0]
     queries = inputs[2:]
     for query in queries:
         if query[0] == 1:
-            sequence = apply_query_1(sequence, *query[1:])
+            i = query[1]
+            j = query[2]
+            k = query[3]
+            for index in range(i-1, j):
+                sequence[index] += k
         else:
-            apply_query_2(sequence, query[1])
+            print(sequence[query[1]-1])
 
     return
 
