4fa840f29e81894e9252a47d8a6d0da3a56189f8
waegaein <waegaein@gmail.com>
2019-08-15 16:58:00 +0900

    Fail 1221

--- fail/1221/run.py
+++ fail/1221/run.py
@@ -1,41 +1,44 @@
 import sys
+import time
 
 def parse_input():
-    f = sys.stdin
+    # f = sys.stdin
     with open('test.txt', 'r') as f:
-        return [tuple(int(coordinate) for coordinate in line.strip().split()) for line in f.readlines()[1:]]
+        return [tuple(int(num) for num in line.split()) for line in f.read().strip().split('\n')[1:]]
 
-def get_distance_square(coordinates1, coordinates2):
-    distance_square = 0
-    for i in range(3):
-        distance_square += (coordinates1[i] - coordinates2[i]) ** 2
+def get_unique_points(points):
 
-    return distance_square
+    return list(dict.fromkeys(points))
 
-def get_unique_coordinates(coordinates):
+def get_distance_square(point_from, point_to):
+    diff_x = point_from[0] - point_to[0]
+    diff_y = point_from[1] - point_to[1]
+    diff_z = point_from[2] - point_to[2]
 
-    return list(set(coordinates))
+    return diff_x * diff_x + diff_y * diff_y + diff_z * diff_z
 
-def run():
-    coordinates = get_unique_coordinates(parse_input())
-    coordinates_length = len(coordinates)
-    minimum_distance_square = None
-    minimum_distance_square_count = 0
+def get_min_distance_with_num_pairs():
+    distances = list()
+    points = get_unique_points(parse_input())
+    num_points = len(points)
 
-    for i in range(coordinates_length-1):
-        for j in range(i+1, coordinates_length):
-            distance_square = get_distance_square(coordinates[i], coordinates[j])
-            if minimum_distance_square is None or distance_square < minimum_distance_square:
-                minimum_distance_square = distance_square
-                minimum_distance_square_count = 1
-            elif distance_square == minimum_distance_square:
-                minimum_distance_square_count += 1
-            else:
-                pass
+    min_distance = None
+    min_distance_num = None
 
-    print(minimum_distance_square)
-    print(minimum_distance_square_count)
+    for i in range(num_points-1):
+        for j in range(i+1, num_points):
+            distance = get_distance_square(points[i], points[j])
+
+            if min_distance is None or distance < min_distance:
+                min_distance = distance
+                min_distance_num = 1
+
+            elif distance == min_distance:
+                min_distance_num += 1
+
+    print(min_distance)
+    print(min_distance_num)
 
     return
 
-run()
+get_min_distance_with_num_pairs()
